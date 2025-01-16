bfde512908f4a94f000a8d5153b9e848a147355b
Divyanshgupta030 <145568562+Divyanshgupta030@users.noreply.github.com>
2025-01-16 23:55:45 +0530

    add sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,16 @@ export default {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
   		borderRadius: {
@@ -64,20 +74,32 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-		 keyframes: {
-        appear: {
-          "0%": { opacity: "0", transform: "translateY(10px)" },
-          "100%": { opacity: "1", transform: "translateY(0)" }
-        },
-        "appear-zoom": {
-          "0%": { opacity: "0", transform: "scale(0.95)" },
-          "100%": { opacity: "1", transform: "scale(1)" }
-        }
-      },
-      animation: {
-        appear: "appear 0.5s ease-out forwards",
-        "appear-zoom": "appear-zoom 0.5s ease-out forwards"
-	  }
+  		keyframes: {
+  			appear: {
+  				'0%': {
+  					opacity: '0',
+  					transform: 'translateY(10px)'
+  				},
+  				'100%': {
+  					opacity: '1',
+  					transform: 'translateY(0)'
+  				}
+  			},
+  			'appear-zoom': {
+  				'0%': {
+  					opacity: '0',
+  					transform: 'scale(0.95)'
+  				},
+  				'100%': {
+  					opacity: '1',
+  					transform: 'scale(1)'
+  				}
+  			}
+  		},
+  		animation: {
+  			appear: 'appear 0.5s ease-out forwards',
+  			'appear-zoom': 'appear-zoom 0.5s ease-out forwards'
+  		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
